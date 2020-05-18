666011ab89df796af1ec68c636ef170b4e35a955
Holic75 <holic.denis@gmail.com>
2020-05-18 21:29:20 +0200

    uncertanity is now calculated only once per enemy/unit pair

--- Considerations.cs
+++ Considerations.cs
@@ -1,6 +1,7 @@
 ﻿using Kingmaker.Controllers.Brain;
 using Kingmaker.Controllers.Brain.Blueprints.Considerations;
 using Kingmaker.EntitySystem.Entities;
+using Kingmaker.PubSubSystem;
 using Kingmaker.RuleSystem;
 using Kingmaker.RuleSystem.Rules;
 using System;
@@ -14,6 +15,7 @@ namespace KingmakerAI.NewConsiderations
 {
     public class AcConsideration : Consideration
     {
+        static Dictionary<(UnitEntityData, UnitEntityData), int> uncertainty_values = new Dictionary<(UnitEntityData, UnitEntityData), int>();
         static System.Random rng = new System.Random();
 
         public float min_score = 0.1f;
@@ -25,6 +27,10 @@ namespace KingmakerAI.NewConsiderations
             UnitEntityData attacker = context.Unit;
             UnitEntityData target = context.Target.Unit ?? context.Unit;
 
+            if (attacker == null || target == null)
+            {
+                return min_score;
+            }
             var weapon = attacker.Body?.PrimaryHand?.MaybeWeapon;
 
             if (!attacker.Body.HandsAreEnabled && attacker.Body.AdditionalLimbs.Count > 0)
@@ -39,13 +45,33 @@ namespace KingmakerAI.NewConsiderations
                 odds = attack_bonus - target_ac;
             }
 
-            var uncertanity_base = (float)(5 + Math.Max(-4, (attacker.Stats.Intelligence.Bonus + attacker.Stats.Wisdom.Bonus)/2));
-            int uncertainty_range = (int)(50.0f / uncertanity_base); //so if bonus is 0, it will be 10, if -4, it will be 50, if +5 it will be + 5
             int uncertainty = 0;
-            if (uncertanity_base != 0)
+            if (uncertainty_values.ContainsKey((attacker, target)))
             {
-                uncertainty = rng.Next(0, uncertainty_range);
+                uncertainty = uncertainty_values[(attacker, target)];
             }
+            else
+            {
+                float uncertanity_base = 0.0f;
+                float sanity = Math.Max(-4, (attacker.Stats.Intelligence.Bonus + attacker.Stats.Wisdom.Bonus) / 2);
+                if (sanity >= 0)
+                {
+                    uncertanity_base = (float)(2.0 + 0.5f*sanity); // 10 for 0,  7 for +2,  5 for + 4,   2 for + 16
+                }
+                else
+                {
+                    uncertanity_base = 2.0f + 0.25f * sanity; //20 for -4, 14 for -2, 10 for 0
+                }
+                //20 for -4, 13 for -2, 10 for 0, 7 for +2,  5 for +4, 4 for +6, 2 for +16
+                
+                int uncertainty_range = (int)Math.Round(20.0f / uncertanity_base);               
+                if (uncertanity_base != 0)
+                {
+                    uncertainty = rng.Next(0, uncertainty_range);
+                }
+                uncertainty_values[(attacker, target)] = uncertainty;
+            }
+
 
             odds = Math.Min(Math.Max(odds, -20), 0); //from -20 to 0
             odds += uncertainty;
@@ -59,6 +85,30 @@ namespace KingmakerAI.NewConsiderations
             }
 
             return Math.Max(Math.Min(score, max_score), min_score);
+        }
+
+        public static void clearUncertanityValues(UnitEntityData unit)
+        {
+
+            foreach (var k in uncertainty_values.Keys.ToArray())
+            {
+                if (unit == k.Item1 || unit == k.Item2)
+                {
+                    uncertainty_values.Remove(k);
+                }
+            }
+            //Main.logger.Log("Stored uncertanities: " + uncertainty_values.Count.ToString());
+        }
+    }
+
+
+    [Harmony12.HarmonyPatch(typeof(UnitEntityData))]
+    [Harmony12.HarmonyPatch("LeaveCombat", Harmony12.MethodType.Normal)]
+    class BlueprintAbility__LeaveCombat__Patch
+    {
+        static void Postfix(UnitEntityData __instance)
+        {
+            AcConsideration.clearUncertanityValues(__instance);
         }
     }
 }
